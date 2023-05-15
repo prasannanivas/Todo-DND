aca245b39a7c1c471864e51aa65d1909e6deb739
Prasanna Nivas <prasannanikvasganguly@gmail.com>
2023-05-14 23:42:54 -0400

    press enter to submit and shift+enter to nextlyn

--- src/components/inputCard/InputCard.js
+++ src/components/inputCard/InputCard.js
@@ -1,39 +1,58 @@
-import { Clear } from '@mui/icons-material';
-import React, { useContext } from 'react'
-import { useState } from 'react';
-import storeAPI from '../../utils/storeAPI';
-import "./styles.scss"
+import { Clear } from "@mui/icons-material";
+import React, { useContext } from "react";
+import { useState } from "react";
+import storeAPI from "../../utils/storeAPI";
+import "./styles.scss";
 
-function InputCard({setOpen, listId, type}) {
-    const [title, setTitle] = useState('');
-    const {addMoreCard , addMoreList} = useContext(storeAPI);
+function InputCard({ setOpen, listId, type }) {
+  const [title, setTitle] = useState("");
+  const { addMoreCard, addMoreList } = useContext(storeAPI);
 
-    const handleSubmit = () => {
-        console.log("here" , type , title);
-        if(type === "card"){
-            addMoreCard(title, listId);
-        }
-        else{
-            addMoreList(title);
-        }
-        setOpen(false);
-        setTitle("");
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter" && !event.shiftKey) {
+      handleSubmit();
     }
+  };
+
+  const handleSubmit = () => {
+    console.log("here", type, title);
+    if (type === "card") {
+      addMoreCard(title, listId);
+    } else {
+      addMoreList(title);
+    }
+    setOpen(false);
+    setTitle("");
+  };
   return (
-    <div className='input-card'>
-        <div className='input-card-container'>
-            <textarea className='input-text' autoFocus placeholder={type==="card" ? "Enter a title" : "Name this List"} value={title} onChange={e => {setTitle(e.target.value)} } />
-        </div>
-        <div className='confirm'>
-            <button className='button-confirm' onClick={handleSubmit}>
-                {type === "card" ? "Add card" : "Add List"}
-            </button>
-            <button className='button-cancel' onClick={()=>{setOpen(false)}}>
-                <Clear/>
-            </button>
-        </div>
+    <div className="input-card">
+      <div className="input-card-container">
+        <textarea
+          className="input-text"
+          autoFocus
+          placeholder={type === "card" ? "Enter a title" : "Name this List"}
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+          onKeyDown={handleKeyDown}
+        />
+      </div>
+      <div className="confirm">
+        <button className="button-confirm" onClick={handleSubmit}>
+          {type === "card" ? "Add card" : "Add List"}
+        </button>
+        <button
+          className="button-cancel"
+          onClick={() => {
+            setOpen(false);
+          }}
+        >
+          <Clear />
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default InputCard
+export default InputCard;
